a092d40a235ceca3e37e62d7cffb0254a8ffb0db
Thiago Alencar <talencar@gmail.com>
2022-03-19 14:14:00 -0300

    First commit. Initial structure.

---
+++ src/com/dio/exercicios/ContaCorrente.java
@@ -0,0 +1,28 @@
+package com.dio.exercicios;
+
+public class ContaCorrente {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar() {
+    }
+
+    public void depositar() {
+    }
+
+    public void transferir() {
+    }
+}
