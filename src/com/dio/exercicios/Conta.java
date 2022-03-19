3c96c8e3bdd1197f057ae48f6bad7e67db703d6e
Thiago Alencar <talencar@gmail.com>
2022-03-19 14:18:13 -0300

    Create Conta superclass.

---
+++ src/com/dio/exercicios/Conta.java
@@ -0,0 +1,29 @@
+package com.dio.exercicios;
+
+public class Conta {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar() {
+    }
+
+    public void depositar() {
+    }
+
+    public void transferir() {
+    }
+
+}
--- src/com/dio/exercicios/ContaPoupanca.java
+++ src/com/dio/exercicios/ContaPoupanca.java
@@ -1,28 +1,5 @@
 package com.dio.exercicios;
 
-public class ContaPoupanca {
-    private int agencia;
-    private int numero;
-    private double saldo;
+public class ContaPoupanca extends Conta {
 
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void sacar() {
-    }
-
-    public void depositar() {
-    }
-
-    public void transferir() {
-    }
 }
